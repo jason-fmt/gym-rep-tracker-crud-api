7db5940a926ca0a5e8af076ac705b5d3e5d09d49
Jason M <jason.moposita1201@gmail.com>
2025-02-05 21:45:37 -0500

    Connect to db and set up route

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from 'express'
+import connectDB from './config/database.js'
+import exerciseRoute from './routes/exercise.route.js'
 
 const app = express()
 
+// middleware
+app.use(express.json())
+
+// connect to database
+connectDB()
+
+// routes
+app.use('/api/exercises', exerciseRoute)
+
+// Root
 app.get('/', (req, res) => {
-   res.send('Root directory')
+   res.send(req.body)
+   console.log(req.body)
 })
 
+// start server
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
    console.log(`Server is running on http://localhost:${PORT}`)
